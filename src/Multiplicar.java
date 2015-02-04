8fb0b3bc3fabda535b050445af5636b12f2718d4
Franksoft24 <frankmer95@gmail.com>
2015-02-04 14:45:55 -0400

    Mejora en el estilo de la aplicacion

--- src/Multiplicar.java
+++ src/Multiplicar.java
@@ -8,24 +8,39 @@ import java.util.*;
 import javax.servlet.*;
 import javax.servlet.http.*;
 public class Multiplicar extends HttpServlet {
-	//private static final long serialVersionUID = 1L;
 	public void doGet(HttpServletRequest req, HttpServletResponse res) 
 				throws ServletException, IOException 
 	{
 		res.setContentType("text/html");
 		PrintWriter pw = res.getWriter();
-		int num = Integer.parseInt(req.getParameter("num"));
+		int num1 = Integer.parseInt(req.getParameter("num1"));
+		int num2 = Integer.parseInt(req.getParameter("num2"));
 		pw.println("<!Doctype html><HTML><HEAD><meta charset=\"utf-8\"><TITLE>Tabla de multiplicar</TITLE>");
-		pw.println("<link rel=\"stylesheet\" href=\"../../src/css/reset.css\">");
-		pw.println("<link rel=\"stylesheet\" href=\"../../src/css/style.css\"></head>");
-		pw.println("<BODY");
-		pw.println("<header>Tabla de multiplicar</header><section class=\"tabla\"><table>");
+		pw.println("<style>table{float:left; padding:5%; background-color:#fff;}body{background-color:#ECECEC}header{margin-left:10%;margin-right:10%;width:80%;background-color:#fff;text-align:center;text-transform:uppercase;}section{margin-right:10%;margin-left:10%;width:80%;background-color:#fff;overflow:hidden;margin-top:3%;}</style>");
+		pw.println("<link rel=\"stylesheet\" type=\"text/css\" href=\"recursos/css/style.css\" /></head>");
+		pw.println("<body>");
+		pw.println("<header>Tabla de multiplicar</header><section class=\"tabla\">");
 		int multiplicar;
-		for(int i=0; i <= 20; i++){
-			multiplicar = i*num;
-			pw.println("<tr><td>"+i+"x"+num+": "+multiplicar+"</td></tr>");
+		if(num1<=num2){
+			for(int j = num1; j <= num2; j++){
+				pw.println("<table>");
+				for(int i = 1; i <=20; i++){			
+					multiplicar = i*j;
+					pw.println("<tr><td>"+j+"x"+i+": "+multiplicar+"</td></tr>");
+				}
+				pw.println("</table>");
+			}
+		}else{
+			for(int j = num2; j <= num1; j++){
+				pw.println("<table>");
+				for(int i = 1; i <=20; i++){			
+					multiplicar = i*j;
+					pw.println("<tr><td>"+j+"x"+i+": "+multiplicar+"</td></tr>");
+				}
+				pw.println("</table>");
+			}
 		}
-		pw.println("</table></section></body></html>");
+		pw.println("</section></body></html>");
 		pw.close();
 	}
 } 
